60b907f8b22f4bebac92017187086c6c900236b7
wangtseng <little.ignorance@gmail.com>
2020-01-10 10:47:23 +0800

    hehe

--- src/py-dev/RCPControl/Motor/CatheterOrientalMotor.py
+++ src/py-dev/RCPControl/Motor/CatheterOrientalMotor.py
@@ -254,7 +254,7 @@ class CatheterOrientalMotor(AdvanceMotor):
                 self.context.setGlobalParameter(self.hapticFeedbackID, self.expectedSpeed)
             time.sleep(0.03)
 
-
+"""
 motor1 = CatheterOrientalMotor()
 #motor1.set_position(50)
 #motor1.set_pos_mode_expectedSpeed(-5)
@@ -265,4 +265,4 @@ motor1.stop()
 motor1.position_move()
 print(time.time()-start)
 #motor1.stop()
-
+"""
