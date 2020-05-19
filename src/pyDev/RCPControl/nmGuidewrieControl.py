550ad27c7d6d5946c00211d0f0af56ff2cea0fa4
wangtseng <little.ignorance@gmail.com>
2020-05-19 11:57:00 +0800

    Motor

--- src/pyDev/RCPControl/nmGuidewrieControl.py
+++ src/pyDev/RCPControl/nmGuidewrieControl.py
@@ -160,7 +160,7 @@ class nmGuidewireControl(QObject):
                 #    print x
                 with open(path, 'a+') as f:
                     csv_writer = csv.writer(f)
-                    csv_writer.writerow(var)
+                    csv_writer.writerow(str(var))
                     # f.write(tmpData[0])
             time.sleep(0.01)
 
