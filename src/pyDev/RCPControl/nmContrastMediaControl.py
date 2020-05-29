8657832d05b5c8a5247e4ac7f6af0dcb25859cc4
quanzeng-lc <quanzeng_lc@163.com>
2020-05-29 15:23:04 +0800

    test advance home

--- src/pyDev/RCPControl/nmContrastMediaControl.py
+++ src/pyDev/RCPControl/nmContrastMediaControl.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import QObject, pyqtSignal
-from Motor.AngioOrientalMotor import AngioOrientalMotor
+from RCPControl.Motor.AngioOrientalMotor import AngioOrientalMotor
 
 
 class nmContrastMediaControl(QObject):
