fcb81da669f820232cc540417d624f0c843c9472
quanzeng-lc <quanzeng_lc@163.com>
2020-06-16 16:29:24 +0800

    guidewire distance

--- src/pyDev/RCPControl/nmGuidewireControl.py
+++ src/pyDev/RCPControl/nmGuidewireControl.py
@@ -122,7 +122,7 @@ class nmGuidewireControl(QObject):
         self.guidewireProgressMotor.get_current_position()
 
     def get_guidewire_absolute_position(self):
-        if self.guidewire_status == 2:
+        if self.guidewire_status == 2 or self.guidewire_status == 5:
             return self.guidewire_round_dst    #mm
         else:
             return self.guidewire_round_dst + self.get_guidewire_position()
