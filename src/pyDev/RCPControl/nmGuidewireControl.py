4fc63cd1a6baf2e41b252eaac793bfbf67fcdf3d
quanzeng-lc <quanzeng_lc@163.com>
2020-06-17 17:48:26 +0800

    test guidewire

--- src/pyDev/RCPControl/nmGuidewireControl.py
+++ src/pyDev/RCPControl/nmGuidewireControl.py
@@ -1,19 +1,19 @@
 from PyQt5.QtCore import QObject, pyqtSignal
-from RCPControl.Motor.AdvanceOrientalMotor import AdvanceOrientalMotor
-from RCPControl.Motor.RotateOrientalMotor import RotateOrientalMotor
-from RCPControl.Gripper import Gripper
-from RCPControl.ForceSensor import ForceSensor
-from RCPControl.InfraredReflectiveSensor import InfraredReflectiveSensor
-from RCPControl.SensingParameter import SensingParameter
+# from RCPControl.Motor.AdvanceOrientalMotor import AdvanceOrientalMotor
+# from RCPControl.Motor.RotateOrientalMotor import RotateOrientalMotor
+# from RCPControl.Gripper import Gripper
+# from RCPControl.ForceSensor import ForceSensor
+# from RCPControl.InfraredReflectiveSensor import InfraredReflectiveSensor
+# from RCPControl.SensingParameter import SensingParameter
 import time
 import threading
 import csv
-# from Motor.AdvanceOrientalMotor import AdvanceOrientalMotor
-# from Motor.RotateOrientalMotor import RotateOrientalMotor
-# from Gripper import Gripper
-# from ForceSensor import ForceSensor
-# from InfraredReflectiveSensor import InfraredReflectiveSensor
-# from SensingParameter import SensingParameter
+from Motor.AdvanceOrientalMotor import AdvanceOrientalMotor
+from Motor.RotateOrientalMotor import RotateOrientalMotor
+from Gripper import Gripper
+from ForceSensor import ForceSensor
+from InfraredReflectiveSensor import InfraredReflectiveSensor
+from SensingParameter import SensingParameter
 
 
 class nmGuidewireControl(QObject):
